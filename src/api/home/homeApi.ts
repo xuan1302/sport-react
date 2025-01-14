f64904fc248a601f636adc22668916e7b1572c72
xuan1302 <xuaan.ndx@gmail.com>
2025-01-14 23:09:56 +0700

    call api home

--- src/api/home/homeApi.ts
+++ src/api/home/homeApi.ts
@@ -5,6 +5,10 @@ const homeApi = {
     const url = "/v1/sporty-shop/home/outstanding-products";
     return axiosClient.get(url);
   },
+  getTopProduct(params: { type: string }): Promise<unknown> {
+    const url = "/v1/sporty-shop/home/top-category";
+    return axiosClient.get(url, { params });
+  },
 };
 
 export default homeApi;
