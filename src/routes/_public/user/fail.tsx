b468990d86b4f07554954314f7667d27540c8fc9
Ninhhh <ninhtx789@gmail.com>
2025-01-17 22:29:24 +0700

    faill

--- src/routes/_public/user/fail.tsx
+++ src/routes/_public/user/fail.tsx
@@ -15,10 +15,10 @@ function RouteComponent() {
       <CloseCircleFilled className='text-red-500' style={{ fontSize: '60px' }}/>
       </div>
       <h1 className="text-2xl font-bold text-orange-500" >
-        Gửi đơn hàng thành công
+        Gửi đơn hàng Thất Bại
       </h1>
       <p className="text-gray-600">
-        Cảm ơn quý khách đã mua hàng tại Aobongda.net. <br />
+        Cảm ơn quý khách đã mua hàng tại vui lòng thực hiện lại <br />
         Nếu bạn có bất kỳ thắc mắc hay câu hỏi nào vui lòng gọi điện để được
         tư vấn: <span className="text-red-500 font-bold">0977500358</span>
       </p>
